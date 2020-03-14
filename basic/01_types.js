a7779b8cdd0e80b19eb0daa777ad0a9b32920420
asosunoff <al.sosunoff@gmail.com>
2020-03-14 10:36:54 +0300

    update

--- basic/01_types.js
+++ basic/01_types.js
@@ -23,3 +23,16 @@ console.log(new Boolean(undefined));
 console.log(new Boolean(NaN));
 console.log(new Boolean(false));
 console.log('\n=======');
+
+/* Приведение к boolean
+ * либое из ниже перечисленных приводится к FALSE
+ * '', 0, null, undefined, NaN, false
+ */
+console.log('=======\n');
+console.log(new Boolean(''));
+console.log(new Boolean(0));
+console.log(new Boolean(null));
+console.log(new Boolean(undefined));
+console.log(new Boolean(NaN));
+console.log(new Boolean(false));
+console.log('\n=======');
